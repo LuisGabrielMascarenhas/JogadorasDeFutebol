fd01d09132e8657c8675daba9266d4c2e7ace898
LuisGabrielMascarenhas <luis.santos614@etec.sp.gov.br>
2023-08-15 22:00:50 -0300

    MyAdapter atualizações no ViewHolder

--- app/src/main/java/br/com/etecia/myapp/MyAdapter.java
+++ app/src/main/java/br/com/etecia/myapp/MyAdapter.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -26,7 +29,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         LayoutInflater inflater = LayoutInflater.from(mContexto);
         view = inflater.inflate(R.layout.modelo_jogadora,parent,false);
 
-        return new RecyclerView.ViewHolder();
+        return new ViewHolder(view);
 
     }
 
@@ -38,5 +41,18 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView nomeJogadoras;
+        ImageView imgJogadoras;
+        CardView idCardJogadoras;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            nomeJogadoras = itemView.findViewById(R.id.nomeJogadoras);
+            imgJogadoras = itemView.findViewById(R.id.imgJogadoras);
+            idCardJogadoras = itemView.findViewById(R.id.idcardJogadoras);
+        }
     }
 }
