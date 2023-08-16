5294bc6776d97d731a86634b7ca88c8570725a00
LuisGabrielMascarenhas <luis.santos614@etec.sp.gov.br>
2023-08-15 21:25:09 -0300

    modelo do layout contendo as informações (incompleto)

---
+++ app/src/main/java/br/com/etecia/myapp/ApresentaJogadoras.java
@@ -0,0 +1,17 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class ApresentaJogadoras extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.apresenta_jogadoras_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/myapp/Jogadoras.java
@@ -0,0 +1,6 @@
+package br.com.etecia.myapp;
+
+public class Jogadoras {
+    private String nome;
+    
+}
